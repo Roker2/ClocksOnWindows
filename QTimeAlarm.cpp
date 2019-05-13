366e20e56c853817b92b6a4110968b69e80a5349
Roker2 <dimaminko2000@gmail.com>
2019-05-13 17:13:20 +0300

    QTimeAlarm: Add SetCurrentTime to timerEvent

--- QTimeAlarm.cpp
+++ QTimeAlarm.cpp
@@ -3,9 +3,10 @@
 QTimeAlarm::QTimeAlarm(QObject *parent, QLabelClocks *Clocks) : QObject(parent)
 {
     LabelClocks = Clocks;
+    startTimer(1000);
 }
 
 void QTimeAlarm::timerEvent(QTimerEvent *)
 {
-
+    LabelClocks->SetCurrentTime();
 }
