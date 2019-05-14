0dd53fa562fccf3e0a262eb0b5fe065c1795eaab
Roker2 <dimaminko2000@gmail.com>
2019-05-14 13:20:53 +0300

    QTimeAlarm: Change timer time and fix alarm

--- QTimeAlarm.cpp
+++ QTimeAlarm.cpp
@@ -3,7 +3,7 @@
 QTimeAlarm::QTimeAlarm(QObject *parent, QLabelClocks *Clocks) : QObject(parent)
 {
     LabelClocks = Clocks;
-    startTimer(1);
+    startTimer(1000);
 }
 
 void QTimeAlarm::timerEvent(QTimerEvent *)
@@ -11,7 +11,7 @@ void QTimeAlarm::timerEvent(QTimerEvent *)
     LabelClocks->SetCurrentTime();
     if (SettingsMenu->AlarmState)
     {
-        if (QTime::currentTime() == SettingsMenu->AlarmTime())
+        if (QTime::currentTime().toString("hh:mm:ss") == SettingsMenu->AlarmTime().toString("hh:mm:ss"))
         {
             QMessageBox::information(nullptr, "Tutturu!", "Alarm");
             SettingsMenu->DisableAlarm();
